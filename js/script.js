68a69003a123eaa4eb3e267ead9a1c146c9cca88
Saw Thinkar <sawthinkarnayhtoo@gmail.com>
2015-10-28 13:14:49 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 confirm("I feel awesome!")
 confirm("I am ready to go.")
 confirm("Nice to meet you, I am Saw Thinkar Nay Htoo.")
+function processForm() {
+    
+    var temperature = Number(document.tempForm.temp.value);
+    var tempType;
+    var result;
+
+    for (var i=0; i < document.tempForm.choice.length; i++) {
+        
+        if (document.tempForm.choice[i].checked) {
+            tempType = document.tempForm.choice[i].value;
+        }
+    }
+
+    if (tempType == 'fahrenheit') {
+        result = temperature * 9/5 + 32;
+    }
+    
+    else {
+        result = (temperature -  32)  *  5/9;
+    }
+    
+    // Assign the result field value here
+}
